616eee5807f7797038275e1970b484f0d25a2609
WEB@Yukio <90124080+web-y24@users.noreply.github.com>
2025-11-16 17:11:27 +0900

    Gulp ハンズオン：Step1 — SCSS コンパイル

--- hands-on-gulp/gulpfile.mjs
+++ hands-on-gulp/gulpfile.mjs
@@ -1,4 +1,14 @@
-import { series } from "gulp";
+import { src, dest, series } from "gulp";
+import gulpSass from "gulp-sass";
+import * as dartSass from "sass";
+
+const sass = gulpSass(dartSass);
+
+export function styles() {
+  return src("src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("dist/css"));
+}
 
 export function noop(done){ done(); }
 
