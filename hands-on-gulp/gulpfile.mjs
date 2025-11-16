f9c79e5f73e5da5d3020cfc6de2ee97adbd2f247
WEB@Yukio <90124080+web-y24@users.noreply.github.com>
2025-11-16 18:02:23 +0900

    Gulp ハンズオン:Step2 — sourcemaps / autoprefixer / minify

--- hands-on-gulp/gulpfile.mjs
+++ hands-on-gulp/gulpfile.mjs
@@ -2,13 +2,33 @@ import { src, dest, series } from "gulp";
 import gulpSass from "gulp-sass";
 import * as dartSass from "sass";
 
+import sourcemaps from "gulp-sourcemaps";
+import autoprefixer from "gulp-autoprefixer";
+import cleanCSS from "gulp-clean-css";
+
+const isProd = process.env.NODE_ENV === "production";
+const isDev = !isProd;
+
 const sass = gulpSass(dartSass);
 
-export function styles() {
-  return src("src/scss/**/*.scss")
+export function stylesDev() {
+  return src("src/scss/**/*.scss", { sourcemaps: true })
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(sourcemaps.write("."))
     .pipe(dest("dist/css"));
 }
+
+export function stylesProd() {
+  return src("src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(dest("dist/css"));
+}
+
+export const styles = isProd ? stylesProd : stylesDev;
 
 export function noop(done){ done(); }
 
